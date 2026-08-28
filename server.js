5f40c8253c14920f58cc036d774d6c09d73d3d71
Aditya <Adityanalawade527@gmail.com>
2026-08-28 12:05:28 +0530

    final push

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const pgp=require('pg-promise')
-const db = pgp('postgres://username:password@host:port/database');
-
-const express=require("express");
-const app=express();
+const express=require("express")
+const app=express()
 app.use(express.json())
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    console.log("Server running on port 3000")
 })
